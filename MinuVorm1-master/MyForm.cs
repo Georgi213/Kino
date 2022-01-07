d6f3b682e15791b6ecdfbef42f07275f6b8cd9d9
Georgi213 <77331773+Georgi213@users.noreply.github.com>
2022-01-07 15:44:52 +0200

    Add files via upload

---
+++ MinuVorm1-master/MyForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MinuVorm
+{
+	public partial class MyForm : Form
+	{
+		readonly TableLayoutPanel tlp = new TableLayoutPanel();
+		readonly Button btn;
+		readonly int w, h;
+		public MyForm() {}
+		
+		public MyForm(int x, int y)
+		{
+			tlp.ColumnCount = x;
+			tlp.RowCount = y;
+			tlp.ColumnStyles.Clear();
+			tlp.RowStyles.Clear();
+			//this.Size = new Size(600, 600);
+			for (int i = 0; i < x; i++)
+			{
+				tlp.ColumnStyles.Add(new ColumnStyle(SizeType.Percent));
+				tlp.ColumnStyles[i].Width = 100 / x;
+			}
+			for (int i = 0; i < y; i++)
+			{
+				tlp.RowStyles.Add(new RowStyle(SizeType.Percent));
+				tlp.RowStyles[i].Height = 100 / y;
+			}
+			for (int i = 0; i < x; i++)
+			{
+				for (int j = 0; j < y; j++)
+				{
+					btn = new Button
+					{
+						Text = $"{i + 1};{j + 1}",
+						Name = $"btn{i + 1}{j + 1}",
+						Dock = DockStyle.Fill
+					};
+					tlp.Controls.Add(btn, i, j);
+				}
+			}
+			w = (100 / x);
+			tlp.Dock = DockStyle.Fill;
+			h = (100 / y);
+			tlp.Size = new Size(tlp.ColumnCount * w*2, tlp.RowCount * h*2);
+			this.Controls.Add(tlp);
+		}
+	}
+}
---
+++ MinuVorm1-master/StartForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinuVorm
+{
+	class StartForm : MyForm
+	{
+		public StartForm()
+		{
+			Label welcome = new Label
+			{
+				Text = "Welcome to cinema 'Amogus'",
+				Dock = DockStyle.Top,
+				TextAlign = ContentAlignment.TopCenter
+					
+			};
+			this.Controls.Add(welcome);
+			Button btn = new Button
+			{
+				Text = "Choose the cinema hall",
+				Location = new Point(80, this.Height / 2),
+				AutoSize = true,
+				BackColor = Color.Red
+			};
+			btn.Click += Btn_Click;
+			this.Controls.Add(btn);
+			/*Button btn2 = new Button
+			{
+				Text = "Choose the cinema hall",
+				Location = new Point(80, this.Height/2),
+				AutoSize = true,
+				BackColor = Color.Red
+			};
+			btn2.Click += Btn2_Click;
+			this.Controls.Add(btn2);*/
+		}
+		/*private void Btn2_Click(object sender, EventArgs e)
+		{
+			MyForm frm = new MyForm(10,5);
+			frm.StartPosition = FormStartPosition.CenterScreen;
+			frm.ShowDialog();
+		}*/
+		private void Btn_Click(object sender, EventArgs e)
+		{
+			CinemaPick frm = new CinemaPick("Choose the hall", "", new string[4] {"Small", "Medium", "Big", "Huge" });
+			frm.StartPosition = FormStartPosition.CenterScreen;
+			frm.Show();
+		}
+	}
+}
